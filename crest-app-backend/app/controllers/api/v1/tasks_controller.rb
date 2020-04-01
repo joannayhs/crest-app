8bcd192711c9db7972ae4366fab083aaf015cbb8
joannayhs <joannayuhas@gmail.com>
2020-04-01 14:30:34 -0400

    added index and show action to task controller

--- crest-app-backend/app/controllers/api/v1/tasks_controller.rb
+++ crest-app-backend/app/controllers/api/v1/tasks_controller.rb
@@ -1,2 +1,12 @@
 class Api::V1::TasksController < ApplicationController
+
+    def index
+        tasks = Task.all 
+        render json: tasks
+    end 
+
+    def show 
+        task = Task.find(params[:id])
+        render json: task 
+    end 
 end
