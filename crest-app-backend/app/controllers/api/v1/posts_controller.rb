114a354c1f3d87dfbb12f6ae828acd3baa041eb6
joannayhs <joannayuhas@gmail.com>
2020-04-01 14:31:53 -0400

    add index and show actiont to post controller

--- crest-app-backend/app/controllers/api/v1/posts_controller.rb
+++ crest-app-backend/app/controllers/api/v1/posts_controller.rb
@@ -1,2 +1,12 @@
 class Api::V1::PostsController < ApplicationController
+
+    def index
+        posts = Post.all 
+        render json: posts, include: [:comments]
+    end 
+
+    def show 
+        post = Post.find(params[:id])
+        render json: post, include: [:comments]
+    end 
 end
